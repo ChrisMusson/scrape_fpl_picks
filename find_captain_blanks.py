2089c4d2a14afa7d5df50e8c35f64cd7fad4fbd7
Chris Musson <chris.musson@hotmail.com>
2023-05-31 16:40:33 +0100

    Add scripts to find captain stats

--- find_captain_blanks.py
+++ find_captain_blanks.py
@@ -3,15 +3,15 @@ import requests
 
 N = 1000  # num users per file
 
-with open("captain_scores.json", "r") as f:
-    user_captain_scores = json.load(f)
+with open("captain_blanks.json", "r") as f:
+    user_captain_blanks = json.load(f)
 
 with open("player_points.json", "r") as f:
     player_points = json.load(f)
 
 with requests.Session() as s:
     for i in range(4000):
-        if str(i * 1000 + 1) in user_captain_scores:
+        if str(i * 1000 + 1) in user_captain_blanks:
             print(f"Skipping {i}")
             continue
 
@@ -24,18 +24,25 @@ with requests.Session() as s:
 
         for j in range(N):
             user_id = i * N + j + 1
-            capt_score = 0
+            capt_blanks = []
             picks = data[j]["picks"]
             for gw, lineup in picks.items():
                 try:
                     cap = [x for x in lineup if x[1] > 1][0]
                     pts = player_points[str(cap[0])][str(gw)]
-                    capt_score += pts * cap[1]
+                    mult = cap[1]
+                    if mult == 2:
+                        if pts < 7:
+                            capt_blanks.append(gw)
+                    if mult == 3:
+                        if pts < 10:
+                            capt_blanks.append(gw)
                 except IndexError:
+                    capt_blanks.append(gw)
                     # print(user_id, gw)  # user didn't have a playing captain for this gameweek so can just ignore
                     continue
-            user_captain_scores[user_id] = capt_score
+            user_captain_blanks[user_id] = capt_blanks
 
-user_captain_scores = dict(sorted(user_captain_scores.items(), key=lambda x: (-x[1], int(x[0]))))
-with open("captain_scores.json", "w") as f:
-    f.write(json.dumps(user_captain_scores))
+user_captain_blanks = dict(sorted(user_captain_blanks.items(), key=lambda x: (len(x[1]), int(x[0]))))
+with open("captain_blanks.json", "w") as f:
+    f.write(json.dumps(user_captain_blanks))
