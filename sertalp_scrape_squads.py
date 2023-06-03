5d128b4750be9f62154fb44a0d9655f47b7ea978
Chris Musson <chris.musson@hotmail.com>
2023-06-03 18:25:31 +0100

    Update squad scraper

--- sertalp_scrape_squads.py
+++ sertalp_scrape_squads.py
@@ -38,18 +38,18 @@ async def update_json(session, filename, start_user_id, end_user_id, current_gw)
     try:
         with open(filename, "r") as f:
             existing_data = json.load(f)
-            start_gw = sorted([int(x) for x in existing_data[0]["picks"].keys()])[-1] + 1
+            # start_gw = sorted([int(x) for x in existing_data[0]["picks"].keys()])[-1] + 1
+            gws = [x for x in range(1, current_gw + 1) if str(x) not in existing_data[0]["picks"].keys()]
     except (FileNotFoundError, json.JSONDecodeError):
         existing_data = None
-        start_gw = 1
+        gws = range(1, current_gw + 1)
 
-    gws = range(start_gw, current_gw + 1)
     urls = [
         f"https://fantasy.premierleague.com/api/entry/{user_id}/event/{gw}/picks/"
         for user_id in user_ids
         for gw in gws
     ]
-    print(f"Getting {len(urls)} urls")
+    # print(f"Getting {len(urls)} urls")
 
     headers = {"User-Agent": f"Android {random.choice(range(7, 20))}.{random.choice(range(1,4))}"}
     tasks = [fetch(session, url, headers) for url in urls]
@@ -63,7 +63,8 @@ async def update_json(session, filename, start_user_id, end_user_id, current_gw)
             user_data["picks"] = {}
         else:
             user_data = existing_data[i]
-        for gw, result in enumerate(results[i * len(gws) : (i + 1) * len(gws)], start=start_gw):
+        for j, result in enumerate(results[i * len(gws) : (i + 1) * len(gws)]):
+            gw = gws[j]
             user_data["picks"][gw] = [[x["element"], x["multiplier"]] for x in result["picks"]]
         data.append(user_data)
 
@@ -79,10 +80,10 @@ async def main():
     session = aiohttp.ClientSession()
     # rotate_VPN(vpn_settings)
 
-    files_dct = {file: os.path.getsize(f"sertalp/{file}") for file in os.listdir(BASE_FOLDER)}
-    files_dct = sorted(files_dct.items(), key=lambda x: x[1])
-    to_do = [int(x[0][:4]) for x in files_dct]
-    print(to_do)
+    # files_dct = {file: os.path.getsize(f"sertalp/{file}") for file in os.listdir(BASE_FOLDER)}
+    # files_dct = sorted(files_dct.items(), key=lambda x: x[1])
+    # to_do = [int(x[0][:4]) for x in files_dct]
+    # print(to_do)
 
     for chunk_num, i in enumerate(range(args.start_chunk, args.end_chunk), start=1):
         # for chunk_num, i in enumerate(to_do, start=1):
