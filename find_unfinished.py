ff3ee8c0babb1cb801d6830955827cc50e85cfdd
Chris Musson <chris.musson@hotmail.com>
2023-06-03 18:26:51 +0100

    Update find_unfinished.py

--- find_unfinished.py
+++ find_unfinished.py
@@ -2,11 +2,20 @@ import json
 
 
 not_done = []
-for i in range(4000):
+for i in range(7450, 7812):
     print(i)
     with open(f"sertalp/{i:04d}.json", "r") as f:
         data = json.load(f)
-        if "chips" not in data[0]:
+        keys = [
+            "first_name",
+            "second_name",
+            "region",
+            "favourite_team",
+            "total_points",
+            "overall_rank",
+            "team_name",
+        ]
+        if not all([x in data[0] for x in keys]):
             not_done.append(i)
             print(f"---------- sertalp/{i:04d}.json ----------")
 
