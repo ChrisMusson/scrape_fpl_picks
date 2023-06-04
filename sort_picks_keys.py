929227477a0e8bd7e8927d5797e87ad826775663
Chris Musson <chris.musson@hotmail.com>
2023-06-04 23:37:35 +0100

    Update key sorting script

--- sort_picks_keys.py
+++ sort_picks_keys.py
@@ -5,7 +5,10 @@ for i in range(4000):
     print(i)
     with open(f"sertalp/{i:04d}.json", "r") as f:
         data = json.load(f)
+
+    if sorted([int(x) for x in data[0]["picks"].keys()]) != list(range(1, 39)):
+        print([int(x) for x in data[0]["picks"].keys()])
         for user in data:
             user["picks"] = dict(sorted(user["picks"].items(), key=lambda x: int(x[0])))
-    with open(f"sertalp/{i:04d}.json", "w") as f:
-        f.write(json.dumps(data))
+        with open(f"sertalp/{i:04d}.json", "w") as f:
+            f.write(json.dumps(data))
