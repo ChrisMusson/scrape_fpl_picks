53098bd5e60a3e088e7d4be8cd603b7f7c436e76
Chris Musson <chris.musson@hotmail.com>
2023-06-05 00:28:47 +0100

    Specify utf-8 encoding and fix pick sorting script

--- sort_picks_keys.py
+++ sort_picks_keys.py
@@ -3,11 +3,10 @@ import json
 
 for i in range(4000):
     print(i)
-    with open(f"sertalp/{i:04d}.json", "r") as f:
+    with open(f"sertalp/{i:04d}.json", "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    if sorted([int(x) for x in data[0]["picks"].keys()]) != list(range(1, 39)):
-        print([int(x) for x in data[0]["picks"].keys()])
+    if [int(x) for x in data[0]["picks"].keys()] != list(range(1, 39)):
         for user in data:
             user["picks"] = dict(sorted(user["picks"].items(), key=lambda x: int(x[0])))
         with open(f"sertalp/{i:04d}.json", "w") as f:
